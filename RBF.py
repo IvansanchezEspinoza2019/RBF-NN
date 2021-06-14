bdc99cf25b540e15ced28afb5d262786c605a577
Ivan Sanchez <edgarse1945@gmail.com>
2021-06-13 21:00:28 -0500

    Propagation and training

--- RBF.py
+++ RBF.py
@@ -4,9 +4,7 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
-
-problem = 2
+problem = 3   # Choose the problem
 
 if problem == 1: # Regression
     ####### Create regression problem ########
@@ -129,3 +127,50 @@ elif problem==2: # Regresion
     plt.legend()
     plt.grid()
     plt.show()
+
+elif problem == 3:
+    def plotData(x, y):
+        ## plot every single pattern of every class in the graph 
+        for i in range(x.shape[1]):
+            if y[0, i] == 1:
+                plt.plot(x[0, i], x[1, i], '.b', markersize=2)   # class 0
+            else:
+                plt.plot(x[0, i], x[1, i], '.r', markersize=2)   # class 1
+        
+    
+    ### Read dataset ### 
+    data = pd.read_csv("moons.csv")
+    # get x and y
+    x = np.asanyarray(data.drop(columns=['y']))
+    y = np.asanyarray(data[['y']])
+    
+    ## number of clusters ##
+    k = 3
+    model = KMeans(n_clusters=k)
+    model.fit(x)
+    
+    c = model.cluster_centers_
+    sigma =  ((np.max(c) - np.min(c)) / np.sqrt(2*k))
+    
+    # Create G matrix (The outputs of every neuron for each pattern)  sahpe: (samples of X, k neurons)
+    G = np.zeros((x.shape[0], k))
+    
+    for i in range(x.shape[0]):
+        for j in range(k):
+            """
+                Gaussian function
+            """
+            distance = np.linalg.norm(x[i, ] - c[j, ], 2)   # norm two (euclidian norm)
+            G[i, j] = np.exp((-1/sigma**2) * distance**2)  # save it on the matrix
+    
+    ### Supervised training ###
+    W = np.dot(np.linalg.pinv(G), y)
+    
+    ypred = np.dot(G, W)
+    
+    # plot dataset
+    plotData(x.T, y.T)
+    
+
+    
+    
