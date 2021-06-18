c3862f886063e23df1da5d2e3907eb4afc939626
Ivan Sanchez <edgarse1945@gmail.com>
2021-06-17 20:32:41 -0500

    RBF-NN Classification problem solved

--- RBF.py
+++ RBF.py
@@ -92,7 +92,7 @@ elif problem==2: # Regresion
     # calculate sigma based on neurons centroids
     sigma =  ((max(c) - min(c)) / np.sqrt(2*k))[0]
     
-    # Create G matrix (The outputs of every neuron for each pattern)  sahpe: (samples of X, k neurons)
+    # Create G matrix (The outputs of every neuron for each pattern)  shape: (N. samples of X, k neurons)
     G = np.zeros((x.shape[1], k))
     
     for i in range(x.shape[1]):
@@ -104,6 +104,7 @@ elif problem==2: # Regresion
             G[i, j] = np.exp((-1/sigma**2) * distance**2)  # save it on the matrix
             
     ### Supervised training ###
+    # pseudo-inverse
     W = np.dot(np.linalg.pinv(G), y.T)
     
     #### prediction ####
@@ -128,7 +129,8 @@ elif problem==2: # Regresion
     plt.grid()
     plt.show()
 
-elif problem == 3:
+elif problem == 3:   ### Clasification
+    
     def plotData(x, y):
         ## plot every single pattern of every class in the graph 
         for i in range(x.shape[1]):
@@ -138,21 +140,32 @@ elif problem == 3:
                 plt.plot(x[0, i], x[1, i], '.r', markersize=2)   # class 1
         
     
-    ### Read dataset ### 
+    ######## Read dataset ########
     data = pd.read_csv("moons.csv")
+    
     # get x and y
     x = np.asanyarray(data.drop(columns=['y']))
     y = np.asanyarray(data[['y']])
     
-    ## number of clusters ##
-    k = 3
+    ## number of clusters (RBF neurons) ##
+    k = 17
+    
+    ####### Unsupervised training #######
     model = KMeans(n_clusters=k)
+    
+    # train kmeans
     model.fit(x)
     
+    ## get centroids ##
     c = model.cluster_centers_
+    
+    ## gaussian function deviation ## 
     sigma =  ((np.max(c) - np.min(c)) / np.sqrt(2*k))
     
-    # Create G matrix (The outputs of every neuron for each pattern)  sahpe: (samples of X, k neurons)
+    #plt.plot(c[:,0], c[:, 1], 'ok')
+    
+    ######## Propagation ##########
+    # Create G matrix (The outputs of every neuron for each pattern)  shape: (N. samples of X, k neurons)
     G = np.zeros((x.shape[0], k))
     
     for i in range(x.shape[0]):
@@ -161,16 +174,57 @@ elif problem == 3:
                 Gaussian function
             """
             distance = np.linalg.norm(x[i, ] - c[j, ], 2)   # norm two (euclidian norm)
-            G[i, j] = np.exp((-1/sigma**2) * distance**2)  # save it on the matrix
+            G[i, j] = np.exp((-1/sigma**2) * distance**2)   
     
-    ### Supervised training ###
+    ######### Supervised training ########
+    # pseudo inverse method
     W = np.dot(np.linalg.pinv(G), y)
-    
-    ypred = np.dot(G, W)
     
     # plot dataset
     plotData(x.T, y.T)
     
-
+    ###### make the decision surface ######
+    ## get 2-dimention min max
+    xmin, ymin = np.min(x[:, 0]) - 0.5, np.min(x[:, 1]) - 0.5
+    xmax, ymax = np.max([x[:, 0]]) + 0.5, np.max(x[:, 1 ]) +0.5
     
+    ## now we have to create a mesh with that limits (100x100) points
+    xx, yy = np.meshgrid(np.linspace(xmin, xmax, 100), np.linspace(ymin, ymax, 100))
+    
+    ## flat xx and yy
+    xx = xx.ravel()
+    yy = yy.ravel()
+
+    ### Propagation ###
+    G = np.zeros((xx.shape[0], k))
+
+    for i in range(xx.shape[0]):
+        for j in range(k):
+            # calculate gaussian function 
+            distance = np.linalg.norm((xx[i], yy[i]) - c[j], 2)
+            G[i, j] = np.exp((-1/sigma**2) * distance**2)
+            
+    # reshape xx and yy to it's original form
+    xx = xx.reshape(100,100)
+    yy = yy.reshape(100,100)
+    
+    ##### Prediction ######
+    zz = np.dot(G, W)
+    
+    # same shape as xx and yy
+    zz = zz.reshape(xx.shape)
+    
+    ## title
+    plt.title("RBF-NN Classification")
+    
+    ## axis limits
+    plt.xlim([xmin, xmax])
+    plt.ylim([ymin, ymax])
+    
+    ## plot contourf
+    plt.contourf(xx, yy, zz)
+    plt.grid()
+    plt.show()  
+
+        
     
